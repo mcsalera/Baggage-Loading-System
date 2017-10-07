a1c082c390ad19555c6b77f6889d3fb5a0331e39
Marie Curie Salera <mcsalera@gmail.com>
2017-10-07 23:30:09 +0800

    initial commit

---
+++ src/main/java/com/uplift/baggageloadingsystem/BaggageLoadingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.uplift.baggageloadingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaggageLoadingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaggageLoadingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uplift/baggageloadingsystem/BaggageLoadingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uplift.baggageloadingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaggageLoadingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
