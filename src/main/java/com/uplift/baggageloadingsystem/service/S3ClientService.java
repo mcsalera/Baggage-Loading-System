578ab367a4641f8d73063ee0d47fea15ff5b4c7b
Jasper Bernales <bernalesjasper@gmail.com>
2017-10-21 23:15:09 +0800

    s3 update

--- src/main/java/com/uplift/baggageloadingsystem/service/S3ClientService.java
+++ src/main/java/com/uplift/baggageloadingsystem/service/S3ClientService.java
@@ -1,7 +1,7 @@
 package com.uplift.baggageloadingsystem.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +12,14 @@ public class S3ClientService {
 
     private AmazonS3 amazonS3;
 
-    @Value("${aws.s3.bucket.name}")
+    @Value("${cloud.aws.s3.bucket.name}")
     private String bucketName;
-
-
 
     S3ClientService(AmazonS3 amazonS3) {
         this.amazonS3 = amazonS3;
     }
 
     void upload(String filename, File file) {
-        TransferManager transferManager = new TransferManager(amazonS3);
-        transferManager.upload(bucketName, filename, file);
+        amazonS3.putObject(new PutObjectRequest(bucketName, filename, file));
     }
 }
