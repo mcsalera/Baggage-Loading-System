980a684cd148307b7850d45a9502b5e7d7731a16
Jasper Bernales <bernalesjasper@gmail.com>
2017-11-05 07:13:07 +0800

    cors

--- src/main/java/com/uplift/baggageloadingsystem/config/MvcConfig.java
+++ src/main/java/com/uplift/baggageloadingsystem/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.uplift.baggageloadingsystem.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,5 +14,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry
             .addResourceHandler("/upload/**")
             .addResourceLocations("file:/tmp/uploads/qrcode/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**");
     }
 }
