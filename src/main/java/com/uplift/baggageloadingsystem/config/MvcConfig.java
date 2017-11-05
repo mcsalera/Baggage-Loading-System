cd30ec01f304a6ea0a536033fcab494ae429bd6b
Jasper Bernales <bernalesjasper@gmail.com>
2017-11-05 09:12:25 +0800

    cors

--- src/main/java/com/uplift/baggageloadingsystem/config/MvcConfig.java
+++ src/main/java/com/uplift/baggageloadingsystem/config/MvcConfig.java
@@ -18,6 +18,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**");
+        registry.addMapping("/**");
     }
 }
